aa8fd6b1e0a0253f4c19e0c08bda958afebca56c
gnoah <ralahajanaharynoah@gmail.com>
2019-08-16 16:08:45 +0300

    test

---
+++ Dashbord/src/components/layout/component/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css'
+
+const About = () => {
+    return (
+      <div className="article">
+        <p>Our goal is to protect our environment</p>
+        <p>Inform people about environment destruction in Madagascar. We invite you to follow the informations into this website.</p>
+      </div>
+    );
+  };
+  
+  export default About;
---
+++ Dashbord/src/components/layout/component/Impact/Impact.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Card from './Card-impact'
+import './Impact.css'
+
+class Impact extends Component {
+  render() {
+    return (
+      <div>
+        <Card />
+      </div>
+    );
+  }
+  };
+
+  export default Impact;
